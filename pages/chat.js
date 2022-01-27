c1f86ccfab0b8c80275c5321986a157f6825efa1
Diego Sano <diegocsano@outlook.com>
2022-01-27 00:41:39 -0300

    Refactor chat page

--- pages/chat.js
+++ pages/chat.js
@@ -1,3 +1,212 @@
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+
+import appConfig from '../config.json';
+
 export default function Chat() {
-  return <h1>Página do Chat</h1>;
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addMessage({ id: uuid(), from: 'Diego Sano', text: message });
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      addMessage({ id: uuid(), from: 'Diego Sano', text: message });
+    }
+  };
+
+  const addMessage = (message) => {
+    setMessages([message, ...messages]);
+    clearTextArea();
+  };
+
+  const clearTextArea = () => {
+    setMessage('');
+  };
+
+  return (
+    <Box
+      styleSheet={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/11/ama-dablam-mountain-1536x864.jpg)`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundBlendMode: 'multiply',
+        color: appConfig.theme.colors.neutrals['000'],
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+          borderRadius: '5px',
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: '100%',
+          maxWidth: '95%',
+          maxHeight: '95vh',
+          padding: '32px',
+        }}
+      >
+        <Header />
+
+        <Box
+          styleSheet={{
+            position: 'relative',
+            display: 'flex',
+            flex: 1,
+            height: '80%',
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: 'column',
+            borderRadius: '5px',
+            padding: '16px',
+          }}
+        >
+          <MessageList messages={messages} />
+
+          <Box
+            as="form"
+            onSubmit={handleSubmit}
+            styleSheet={{
+              display: 'flex',
+              alignItems: 'center',
+              height: '$rem',
+            }}
+          >
+            <TextField
+              placeholder="Insert your message here..."
+              type="textarea"
+              value={message}
+              onChange={handleChange}
+              onKeyPress={handleKeyPress}
+              styleSheet={{
+                width: '100%',
+                height: '100%',
+                border: '0',
+                resize: 'none',
+                borderRadius: '5px',
+                padding: '6px 8px',
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: '12px',
+                color: appConfig.theme.colors.neutrals[200],
+              }}
+            />
+            <Button
+              type="submit"
+              label="Send"
+              styleSheet={{
+                height: '100%',
+              }}
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals['000'],
+                mainColor: appConfig.theme.colors.primary[500],
+                mainColorLight: appConfig.theme.colors.primary[400],
+                mainColorStrong: appConfig.theme.colors.primary[600],
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <Box
+        styleSheet={{
+          width: '100%',
+          marginBottom: '16px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Text variant="heading5">Chat</Text>
+        <Button
+          variant="tertiary"
+          colorVariant="neutral"
+          label="Logout"
+          href="/"
+        />
+      </Box>
+    </>
+  );
+}
+
+function MessageList({ messages }) {
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: 'scroll',
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        flex: 1,
+        color: appConfig.theme.colors.neutrals['000'],
+        marginBottom: '16px',
+      }}
+    >
+      {messages.map((message) => {
+        return (
+          <Text
+            key={message.id}
+            tag="li"
+            styleSheet={{
+              borderRadius: '5px',
+              padding: '6px',
+              marginBottom: '12px',
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+              },
+            }}
+          >
+            <Box
+              styleSheet={{
+                marginBottom: '8px',
+              }}
+            >
+              <Image
+                styleSheet={{
+                  width: '20px',
+                  height: '20px',
+                  borderRadius: '50%',
+                  display: 'inline-block',
+                  marginRight: '8px',
+                }}
+                src={`https://github.com/diegosano.png`}
+              />
+              <Text tag="strong">{message.from}</Text>
+              <Text
+                styleSheet={{
+                  fontSize: '10px',
+                  marginLeft: '8px',
+                  color: appConfig.theme.colors.neutrals[300],
+                }}
+                tag="span"
+              >
+                {new Date().toLocaleDateString()}
+              </Text>
+            </Box>
+            {message.text}
+          </Text>
+        );
+      })}
+    </Box>
+  );
 }
